a99102e8f9b0a763897cecef7c183464c1aa451d
amorgan <adam.morgan013@gov.wales>
2018-11-06 11:51:39 +0000

    Update page meta data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import { Helmet } from 'react-helmet'
 
 import Layout from '../components/layout'
 import Image from '../components/image'
 
 const IndexPage = () => (
   <Layout>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Home | CKE</title>
+      <link rel="canonical" href="http://mysite.com/example" />
+    </Helmet>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import { Helmet } from 'react-helmet'
 
 import Layout from '../components/layout'
 
 const SecondPage = () => (
   <Layout>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Page 2 | CKE</title>
+      <link rel="canonical" href="http://mysite.com/example" />
+    </Helmet>
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
