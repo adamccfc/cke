ee97b14fd4b483af7fd2425ec5fc6ba96b5d2860
amorgan <adam.morgan013@gov.wales>
2018-11-21 09:49:19 +0000

    Initial commit for contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+
+import Layout from '../components/layout'
+
+const Contact = () => (
+  <Layout>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Contact Us | CKe</title>
+      <link rel="canonical" href="http://mysite.com/example" />
+    </Helmet>
+  </Layout>
+)
+
+export default Contact
