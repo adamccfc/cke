83fe4ed431f0da81fc8d47094434cb6f5ed3a473
Adam Morgan <adamccfc@gmail.com>
2018-11-22 20:13:06 +0000

    Add contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Helmet } from 'react-helmet'
 
 import Layout from '../components/layout'
+import Container from '../components/container'
 
 const Contact = () => (
   <Layout>
@@ -10,6 +11,24 @@ const Contact = () => (
       <title>Contact Us | CKe</title>
       <link rel="canonical" href="http://mysite.com/example" />
     </Helmet>
+    <Container>
+      <div className="tagline">Contact</div>
+      <h1>Get in touch</h1>
+      <form name="contact" method="POST" netlify>
+        <p>
+          <label>Your Name: <input type="text" name="name" /></label>
+        </p>
+        <p>
+          <label>Your Email: <input type="email" name="email" /></label>
+        </p>
+        <p>
+          <label>Message: <textarea name="message"></textarea></label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+    </Container>
   </Layout>
 )
 
